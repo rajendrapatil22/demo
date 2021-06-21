c6d0bae0900ea2da27fa05e37e0c7ff69c76c0b2
rajendrap <rajendrap@BXLVNRAJENDRAP>
2021-06-21 19:51:26 +0530

    cd

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,14 +2,29 @@ package com.example;
 
 import com.example.model.ToDo;
 import com.example.repository.ToDoRepository;
+import com.example.service.ToDoService;
+
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class DemoApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
@@ -17,8 +32,20 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+@RequestMapping("/")
+		public String hello()
+		{
+			return "Hello";
+		}
+@Autowired
+private ToDoService toDoService;
 
-    @Bean
+@RequestMapping(value = "/todo", method = RequestMethod.GET)
+public ResponseEntity<List<ToDo>> getAllToDo() {
+    logger.info("Returning all the ToDo´s");
+    return new ResponseEntity<List<ToDo>>(toDoService.getAllToDo(), HttpStatus.OK);
+}
+@Bean
     public CommandLineRunner setup(ToDoRepository toDoRepository) {
         return (args) -> {
             toDoRepository.save(new ToDo("Remove unused imports", true));
